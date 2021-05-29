81f84bc7fd86281823379a88253ebcddae7cefca
Alexei <mralexxxx2012@gmail.com>
2021-05-29 13:36:24 +0300

    finished task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,21 @@ package subtask1
 
 class HappyArray {
 
+    private var test: MutableListIterator<Int>? = null
+
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        if(sadArray.isEmpty()) return sadArray
+        
+        val happyList = sadArray.toMutableList()
+
+        for (index in happyList.indices)
+        for(index in happyList.indices){
+            if(index == happyList.indices.first) continue
+            else if(index >= happyList.indices.last) break;
+            else if (happyList[index] > happyList[index.inc()] + happyList[index.dec()]) happyList.removeAt(index)
+        }
+        
+        return  happyList.toIntArray()
     }
 }
